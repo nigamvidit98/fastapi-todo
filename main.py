893df21600cf28bdda7cbc0eedcf28b1493537d9
nigamvidit98 <viditnigam0@gmail.com>
2026-03-26 23:16:24 +0530

    front end working

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 todos = []
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+@app.get("/")
+def serve_frontend():
+    return FileResponse("frontend/index.html")
 
 @app.get("/")
 def read_root():
